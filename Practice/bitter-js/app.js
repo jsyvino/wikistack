533b795d396c14acc13970d31cd37c32fec10f1b
Jaime Syvino <jsyvino@gmail.com>
2018-03-06 14:22:20 -0600

    added nunjucks, break to watch solution

--- Practice/bitter-js/app.js
+++ Practice/bitter-js/app.js
@@ -1,8 +1,26 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 const vball = require('volleyball');
+const nunjucks= require('nunjucks');
 
 app.use(vball);
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+nunjucks.render('index.html', locals,  function(err, output){
+    console.log(output);
+});
+
 
 app.use(function(req, res, next){
     console.log(req.method + " " + req.path);
