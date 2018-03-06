4d17fe237e9f63f0700b2623ed6b7cf967166a1c
Jaime Syvino <jsyvino@gmail.com>
2018-03-06 14:08:22 -0600

    done with set up and configuration of express

--- Practice/bitter-js/app.js
+++ Practice/bitter-js/app.js
@@ -1,10 +1,18 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
+const vball = require('volleyball');
+
+app.use(vball);
+
+app.use(function(req, res, next){
+    console.log(req.method + " " + req.path);
+    next();
+});
 
 app.get('/', function(req, res){
     res.send('<h1>What on earth am I doing?</h1><p>I don\'t think I\'ll ever understand...<p>');
 })
 
 app.listen(3000, function(){
-    console.log("The server is listening, I TELL YOU");
+    
 })
