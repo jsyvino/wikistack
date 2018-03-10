215b1781644e4a2f018b434c8184943bc437cc2e
Jaime Syvino <jsyvino@gmail.com>
2018-03-09 19:23:16 -0600

    stopping for the weekend...still working on set up, have basic HTML loading

---
+++ wikistack/app.js
@@ -0,0 +1,37 @@
+'use strict';
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+const models = require('./models');
+//const makesRouter = require('./routes');
+//const fs = require('fs');
+const bodyParser = require('body-parser');
+const env = nunjucks.configure('views', { noCache: true }); // where to find the views, caching off
+app.set('view engine', 'html'); // what file extension do our templates have
+app.engine('html', nunjucks.render); // how to render html templates
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true })); // for HTML form submits
+app.use(bodyParser.json()); // would be for AJAX requests
+app.use(express.static('./public'));
+
+app.get('/', function (req, res){
+    res.render('index.html')
+})
+
+
+
+
+
+
+
+models.db.sync({force: true})
+.then(function () {
+    console.log('All tables created!');
+    app.listen(3000, function () {
+        console.log('Server is listening on port 3000!');
+    });
+})
+.catch(console.error.bind(console));
+  
